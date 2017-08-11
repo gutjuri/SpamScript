6b26b1ea76c4b1992ccf1ef53e69471ee198cbb7
Juri <juri.dispan@me.com>
2017-08-11 20:44:50 +0200

    Create SpamScr.java

---
+++ SpamScr.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+
+public class SpamScr {
+	
+	static final double VERSION = 0.1;
+	
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		MainWind w = new MainWind();
+		
+	}
+	
+	public static void send(String s, int anz){
+		if(s.equals("")){
+			s = " ";
+		}
+		s = "\""+s+"\"";
+		String path = ClassLoader.getSystemClassLoader().getResource(".").getPath();
+		if(path.endsWith("/bin/")){
+			path = path.substring(1, path.length()-4)+"spam.vbs";
+		}else{
+			path = path.substring(1)+"spam.vbs";
+		}
+		
+		System.out.println(path);
+		
+		try {
+			Runtime.getRuntime().exec("wscript "+path+" "+s+" "+anz);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
